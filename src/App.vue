fcfc6d1facc6e6a7ebe8be65d6729cd7526ef1c5
Saba Radmehr <saba.rdm15@gmail.com>
2024-12-28 21:44:26 +0330

    firs commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+const name = ref("");
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld />
+  <input v-model="name" type="text" />
+  <div class="hi">Hi {{ name }}</div>
+</template>
+
+<!-- <style scoped>
+.hi {
+  border: solid 1px red;
+}
+</style> -->
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hi">what is your name?</div>
+</template>
+
+<style scoped>
+.hi {
+  border: solid 1px red;
+}
+</style>
